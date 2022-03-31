c0cb9794f21a3ce3f13415cefb9708d4db8e09b9
José Carlos <etijcvivi@gmail.com>
2022-03-31 17:58:58 -0300

    create function telaObrigado

--- botao-submit.js
+++ botao-submit.js
@@ -22,10 +22,16 @@ formulario.addEventListener('submit', e => {
     }
 });
 
-
-
-
-// function submitLink() {
-//     var link = <a href="./thankyou.html"></a>
-//     return link;
-// }
+function telaObrigado(container, valorClassific) {
+    container.innerHTML = `
+            <div class="Obrigado">
+                <img src="./images/illustration-thank-you.svg" alt="Obrigado" class="img_obrigado">
+                <p class="obrigado_result">You selected ${valorClassific} out of 5.</p>
+                <h1 class="obrigado_titulo">Thank you!</h1>
+                <p class="descricao_obrigado">
+                    We appreciate you taking the time to give a rating. 
+                    If you ever need more support, don’t hesitate to get in touch!
+                </p>
+        </div>
+    `;
+}

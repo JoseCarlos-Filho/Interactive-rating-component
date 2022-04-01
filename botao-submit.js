94617b8b7794ef8ff27e92c1b402bd71c26955a4
José Carlos <etijcvivi@gmail.com>
2022-03-31 22:26:33 -0300

    Finished vesion Projeto Interactive Rating

--- botao-submit.js
+++ botao-submit.js
@@ -1,6 +1,10 @@
 const formulario = document.querySelector(".form_botoes");
-const radioEntrada = document.querySelectorAll(".form_radio_button");
+const radioEntrada = document.querySelectorAll(".form_botoes_input");
 const classifica = document.querySelector(".classificacao");
+
+// console.log(formulario);
+// console.log(radioEntrada);
+// console.log(classifica);
 
 let classificaValor;
 
@@ -13,25 +17,35 @@ radioEntrada.forEach(input => {
     });
 });
 
+
 formulario.addEventListener('submit', e => {
     
     e.preventDefault();
+     
 
-    if(!classificaValor){
-        return;
-    }
+    if(!classificaValor)return;
+    
+    telaObrigado(classifica, classificaValor);
+    
 });
 
-function telaObrigado(container, valorClassific) {
+function telaObrigado(container, classificaValor) {
     container.innerHTML = `
             <div class="Obrigado">
                 <img src="./images/illustration-thank-you.svg" alt="Obrigado" class="img_obrigado">
-                <p class="obrigado_result">You selected ${valorClassific} out of 5.</p>
+                <p class="obrigado_result">You selected ${classificaValor} out of 5.</p>
                 <h1 class="obrigado_titulo">Thank you!</h1>
                 <p class="descricao_obrigado">
                     We appreciate you taking the time to give a rating. 
                     If you ever need more support, don’t hesitate to get in touch!
                 </p>
-        </div>
+            </div>
     `;
+}
+
+function clickcor() {
+    const click = document.querySelector(".form_radio_button");
+    
+    click.style.backgroundColor = "red";
+    console.log(click);
 }
